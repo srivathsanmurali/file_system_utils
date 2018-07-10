89bd01519b1349cae2b9c7057f5d375e0b012e7b
Srivathsan Murali <srivathsan92@gmail.com>
2018-07-10 12:06:37 +0200

    Create LICENSE and update mix.exs

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # FileSystemUtils
 
-**TODO: Add description**
+Wrapper around common file system linux utilties.
+
+> **Declaimer**
+>
+> Use at own risk.
+> Make sure you know what you are doing.
+>
+> Might cause unnecessary effects if used without caution.
+
+## Implemented wrapper
++ mount
++ umount
++ lsblk
 
 ## Installation
-
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `file_system_utils` to your list of dependencies in `mix.exs`:
+The package can be installed by adding `file_system_utils` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
@@ -14,8 +24,6 @@ def deps do
   ]
 end
 ```
-
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+The docs can
 be found at [https://hexdocs.pm/file_system_utils](https://hexdocs.pm/file_system_utils).
 
--- mix.exs
+++ mix.exs
@@ -4,19 +4,25 @@ defmodule FileSystemUtils.MixProject do
   def project do
     [
       app: :file_system_utils,
+      name: "file_system_utils",
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
-      docs: [markdown_processor: ExDoc.Markdown.Cmark]
+      docs: [markdown_processor: ExDoc.Markdown.Cmark],
+      source_url: "https://github.com/srivathsanmurali/file_system_utils"
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
+  end
+
+  defp description() do
+    "Wrapper around common file system linux utilties."
   end
 
   # Run "mix help deps" to learn about dependencies.
@@ -25,6 +31,15 @@ defmodule FileSystemUtils.MixProject do
       {:jason, "~> 1.1"},
       {:cmark, "~> 0.6", only: :dev},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package() do
+    [
+      name: "file_system_utils",
+      maintainers: ["Srivathsan Murali"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/srivathsanmurali/file_system_utils"}
     ]
   end
 end
